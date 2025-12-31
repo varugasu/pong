07196648e63dbfcd56cc976f739f7a919657b7d9
varugasu <lvargasnoronha@gmail.com>
2025-12-31 14:08:09 -0300

    feat: move player

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+@export
+var speed = 400
+@export
+var width = 100
+@export
+var height = 20
+
+var screen_size: Vector2
+
+func _draw() -> void:
+	draw_rect(Rect2(Vector2.ZERO, Vector2(width, height)), Color.GREEN)
+	
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		position.x += speed * delta
+	elif Input.is_action_pressed("move_left"):
+		position.x -= speed * delta
+
+	position = position.clamp(Vector2.ZERO, screen_size - Vector2(width, height))
