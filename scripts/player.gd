76eb6fcb4754e06dd775f3a9b08b251d3feff8cc
varugasu <lvargasnoronha@gmail.com>
2025-12-31 15:15:49 -0300

    feat: render on editor

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 
 
@@ -8,7 +9,7 @@ var width = 100
 @export
 var height = 20
 
-var screen_size: Vector2
+var screen_size = Vector2.ZERO
 
 func _draw() -> void:
 	draw_rect(Rect2(Vector2.ZERO, Vector2(width, height)), Color.GREEN)
@@ -20,6 +21,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+
 	if Input.is_action_pressed("move_right"):
 		position.x += speed * delta
 	elif Input.is_action_pressed("move_left"):
